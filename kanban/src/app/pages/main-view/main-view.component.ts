69419ce90db75879c29ae8040f58e38d82049c71
MuhandOsman <mo1981ot@gmail.com>
2022-05-06 21:16:32 +0200

    angular cdk dragDrop added

--- kanban/src/app/pages/main-view/main-view.component.ts
+++ kanban/src/app/pages/main-view/main-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
+
 
 @Component({
   selector: 'app-main-view',
@@ -8,8 +12,32 @@ import { Component, OnInit } from '@angular/core';
 export class MainViewComponent implements OnInit {
 
   constructor() { }
+  //creat columns either dynamic like the following(& add board and column models) or hard code HTML columns(main-view)
+
+  board: Board = new Board("task Board", [
+    new Column("Ideas", ["task1","task2","task3","task4"]),
+    new Column("Research", ["lorem","blabla","any","foo"]),
+    new Column("Todo", ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep']),
+    new Column("Done", ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'])
+  ])
 
   ngOnInit(): void {
   }
 
+  // todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  // done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
